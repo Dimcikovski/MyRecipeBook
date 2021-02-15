a8a16e419ccf1c5597e5369bc5bb775eebc7891f
Gjorgji Dimchikovski <dimcikovski21@outlook.com>
2021-02-15 22:19:45 +0100

    Added sidenav

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,17 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
-
-
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    MaterialModule
-  ],
-  exports: [
-    MaterialModule
-  ]
+  declarations: [SidebarComponent],
+  imports: [CommonModule, MaterialModule],
+  exports: [MaterialModule, SidebarComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
---
+++ client/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../models/menu-options';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+  public links: MenuItem[] = [
+    { label: 'Recipe list', icon: 'library_books', href: 'recipe/list' },
+    { label: 'Add new recipe', icon: 'bookmark_add', href: 'recipe/new' },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
