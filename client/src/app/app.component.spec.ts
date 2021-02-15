7319dca3b2fcb4f7dd824b1feeee6ca81f2d6659
Gjorgji Dimchikovski <dimcikovski21@outlook.com>
2021-02-15 19:51:41 +0100

    Added angular material module

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -20,16 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'recipe-book'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-book');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('recipe-book app is running!');
-  });
 });
